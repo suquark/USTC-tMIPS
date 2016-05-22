c6bd5a912adb3c29775f5d6d5f5e965c6f02bba3
Blealtan <bolee159632@live.com>
2016-05-22 20:55:15 +0800

    Two useful instruction added.

--- MIPS_CPU/MIPS_CPU.srcs/sources_1/control_unit.v
+++ MIPS_CPU/MIPS_CPU.srcs/sources_1/control_unit.v
@@ -41,13 +41,11 @@ module control_unit(
     parameter OP_REG_CALC = 6'b000000;
     parameter OP_LW       = 6'b100011;
     parameter OP_SW       = 6'b101011;
+    parameter OP_ADDI     = 6'b001000;
+    parameter OP_BGTZ     = 6'b000111;
     
     parameter FUNCT_ADD   = 6'b100000;
     parameter FUNCT_JR    = 6'b001000;
-    
-    assign BranchN = 0;
-    assign BranchZ = 0;
-    assign BranchP = 0;
     
     always @(*)
     begin
@@ -60,7 +58,10 @@ module control_unit(
                         MemWrite = 0;
                         ALUControl = A_ADD;
                         ALUSrc = ALUSrc_REG;
-                        RegDst = 1;
+                        RegDst = RegDst_rd;
+                        BranchN = 0;
+                        BranchZ = 0;
+                        BranchP = 0;
                         AddrSrc = AddrSrc_IM;
                         Branch = PCSrcID_NORM;
                     end
@@ -70,7 +71,10 @@ module control_unit(
                         MemWrite = 0;
                         ALUControl = A_NOP;
                         ALUSrc = ALUSrc_REG;
-                        RegDst = 0;
+                        RegDst = RegDst_rt;
+                        BranchN = 0;
+                        BranchZ = 0;
+                        BranchP = 0;
                         AddrSrc = AddrSrc_REG;
                         Branch = PCSrcID_BR;
                     end
@@ -79,8 +83,11 @@ module control_unit(
                         MemtoReg = MemtoReg_ALU;
                         MemWrite = 0;
                         ALUControl = A_NOP;
-                        ALUSrc = 0;
+                        ALUSrc = ALUSrc_REG;
                         RegDst = RegDst_rt;
+                        BranchN = 0;
+                        BranchZ = 0;
+                        BranchP = 0;
                         AddrSrc = AddrSrc_IM;
                         Branch = PCSrcID_NORM;
                     end
@@ -93,6 +100,9 @@ module control_unit(
                 ALUControl = A_ADD;
                 ALUSrc = ALUSrc_IM;
                 RegDst = RegDst_rt;
+                BranchN = 0;
+                BranchZ = 0;
+                BranchP = 0;
                 AddrSrc = AddrSrc_IM;
                 Branch = PCSrcID_NORM;
             end
@@ -103,16 +113,48 @@ module control_unit(
                 ALUControl = A_ADD;
                 ALUSrc = ALUSrc_IM;
                 RegDst = RegDst_rt;
+                BranchN = 0;
+                BranchZ = 0;
+                BranchP = 0;
                 AddrSrc = AddrSrc_IM;
                 Branch = PCSrcID_NORM;
+            end
+            OP_ADDI:     begin
+                RegWrite = 1;
+                MemtoReg = MemtoReg_ALU;
+                MemWrite = 0;
+                ALUControl = A_ADD;
+                ALUSrc = ALUSrc_IM;
+                RegDst = RegDst_rt;
+                BranchN = 0;
+                BranchZ = 0;
+                BranchP = 0;
+                AddrSrc = AddrSrc_IM;
+                Branch = PCSrcID_NORM;
+            end
+            OP_BGTZ:     begin
+                RegWrite = 1;
+                MemtoReg = MemtoReg_ALU;
+                MemWrite = 0;
+                ALUControl = A_ADD;
+                ALUSrc = ALUSrc_IM;
+                RegDst = RegDst_rt;
+                BranchN = 0;
+                BranchZ = 0;
+                BranchP = 1;
+                AddrSrc = AddrSrc_IM;
+                Branch = PCSrcID_BR;
             end
             default:     begin
                 RegWrite = 0;
                 MemtoReg = MemtoReg_ALU;
                 MemWrite = 0;
                 ALUControl = A_NOP;
-                ALUSrc = 0;
+                ALUSrc = ALUSrc_REG;
                 RegDst = RegDst_rt;
+                BranchN = 0;
+                BranchZ = 0;
+                BranchP = 0;
                 AddrSrc = AddrSrc_IM;
                 Branch = PCSrcID_NORM;
             end
